30bc0d9a207cdb523e896216eb33d44e37ab1dec
KingLouie1221 <KingLouie1221@DESKTOP-2BTN93J.local>
2020-12-01 09:32:28 -0800

    Update KingLouie.jpg, KingLouie.jpg.meta, and 154 more files...

---
+++ Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace PathCreation.Examples
+{
+    // Moves along a path at constant speed.
+    // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
+    public class PathFollower : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public float speed = 5;
+        float distanceTravelled;
+        private SpriteRenderer sr;
+        private float lastPosition;
+        private float currentPosition;
+        public Transform transformPos;
+
+        void Start() {
+            if (pathCreator != null)
+            {
+                // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                pathCreator.pathUpdated += OnPathChanged;
+            }
+            sr = GetComponent<SpriteRenderer>();
+
+            lastPosition = transformPos.position.x;
+        }
+
+        void Update()
+        {
+            //Log current position
+            currentPosition = transformPos.position.x;
+            
+
+            if (pathCreator != null)
+            {
+                distanceTravelled += speed * Time.deltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                // Check to see if object is moving left or right
+                //To see if sprite should be flipped
+                if (currentPosition < lastPosition)
+                {
+                    sr.flipX = true;
+                    
+                }
+                else if(currentPosition > lastPosition)
+                {
+                    sr.flipX = false;
+                }
+                else
+                {
+                    
+                    sr.flipX = false;
+                }
+            }
+            //Log position of "last frame"
+            lastPosition = currentPosition;
+            
+        }
+
+        // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+        // is as close as possible to its position on the old path
+        void OnPathChanged() {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+    }
+}
