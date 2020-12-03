f35b56e8c26b022820b8c041c5280583e8020a4b
MatthewolVGD <1707240@mukilteo.wednet.edu>
2020-12-03 15:31:34 -0800

    Got Enemy health working

---
+++ Assets/Matthew/Scripts/EnemyHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public float health;
+    public float damage;
+    float bulletDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "TowerProjectile")
+        {
+            bulletDamage = collision.GetComponent<Projectile>().damage;
+            CheckHealth();
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void CheckHealth()
+    {
+        health -= bulletDamage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
