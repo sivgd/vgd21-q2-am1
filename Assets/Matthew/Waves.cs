b5f83dbb8e62e434e52e1670e093995c04054baa
Gavin <Gavin@DESKTOP-EKQG57Q.local>
2020-12-03 09:48:40 -0800

    Fixes

---
+++ Assets/Matthew/Waves.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Waves : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveIndex = 0;
+    public PathCreation.PathCreator spawnPath;
+    public Transform spawnPoint;
+    public Transform enemyParent;
+
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+            
+        }
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        Debug.Log("Wave Incoming");
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation, enemyParent);
+    }
+}
