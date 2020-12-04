8b2c74da0637abd24f9e0014063c8b20724caaaa
Cadd47 <1707230@mukilteo.wednet.edu>
2020-12-03 18:41:07 -0800

    Update .DS_Store, .DS_Store, and 5 more files...

--- Assets/Taylor/Scripts/CabbageTimer.cs
+++ Assets/Taylor/Scripts/CabbageTimer.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CabbageTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // timer starting number and ending number
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    [SerializeField] Text countdownText;
+
     void Start()
     {
-        
+        currentTime = startingTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // subtracts 1 every second
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        // resets back to 10 once reaching 0
+        if (currentTime <= 0)
+        {
+            currentTime = 10;
+        }
     }
 }
