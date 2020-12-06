55afdfee4d00f1324c75a3a617b7b7a294b0cd2c
Cadd47 <1707230@mukilteo.wednet.edu>
2020-12-06 01:27:26 -0800

    Update Demo.unity, CabbageBar 1.png, and 4 more files...

--- Assets/Taylor/Scripts/CabbageCounter.cs
+++ Assets/Taylor/Scripts/CabbageCounter.cs
@@ -17,6 +17,6 @@ public class CabbageCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        score.text = "Cabbages: " + cabbageAmount;
+        score.text = "" + cabbageAmount;
     }
 }
