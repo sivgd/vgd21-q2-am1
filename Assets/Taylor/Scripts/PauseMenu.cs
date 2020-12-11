84da01a5e193f5758a424dc2c5859dff330d4e5d
Cadd47 <1707230@mukilteo.wednet.edu>
2020-12-10 20:53:57 -0800

    Update Demo.unity, MainMenu.cs, and 2 more files...

--- Assets/Taylor/Scripts/PauseMenu.cs
+++ Assets/Taylor/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -35,6 +36,11 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void RestartGame()
