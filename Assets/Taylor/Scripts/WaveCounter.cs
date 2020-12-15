6140af5a80e168f951922aff091468bc42603b1f
Cadd47 <1707230@mukilteo.wednet.edu>
2020-12-15 09:04:51 -0800

    Update Demo.unity, WaveCounter.cs, and WaveCounter.cs.meta

---
+++ Assets/Taylor/Scripts/WaveCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveCounter : MonoBehaviour
+{
+    public static int waveNumber = 0;
+    Text score;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = GetComponent<Text>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Wave: " + waveNumber;
+    }
+
+}
