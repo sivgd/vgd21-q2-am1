6af69c603598c7dd69cf940f3f7d9a5b22da2023
MatthewolVGD <1707240@mukilteo.wednet.edu>
2020-12-27 14:09:23 -0800

    Added beginnings of Snowstorm

---
+++ Assets/Matthew/Scripts/Hazards.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazards : MonoBehaviour
+{
+
+    bool snowstorm;
+    public int stormChance;
+    public float stormTime;
+    public float rangeMultiplier;
+    public GameObject towerParent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if (Random.Range(1, 1) <= stormChance)
+        {
+            Snowstorm();
+        }
+
+
+
+    }
+
+
+    void Snowstorm()
+    {
+        
+        float originalRange = towerParent.GetComponentInChildren<Tower>().range;
+        while (stormTime > 0)
+        {
+            
+            towerParent.GetComponentInChildren<Tower>().range = originalRange * rangeMultiplier;
+
+            stormTime -= Time.deltaTime;
+        }
+
+    }
+}
