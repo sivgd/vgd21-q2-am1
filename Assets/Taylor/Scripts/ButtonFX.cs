295f6601ee0f4cf28c9ff6dbe0050ba59bcf63eb
Cadd47 <1707230@mukilteo.wednet.edu>
2021-01-13 13:13:28 -0800

    Update CreditScene.unity, Demo.unity, and 11 more files...

--- Assets/Taylor/Scripts/ButtonFX.cs
+++ Assets/Taylor/Scripts/ButtonFX.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class SoundManagerScript : MonoBehaviour
+public class ButtonFX : MonoBehaviour
 {
     public AudioSource Fx;
     public AudioClip buttonHover;
